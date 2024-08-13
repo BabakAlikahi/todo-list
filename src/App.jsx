c9bdf398b090e1da7a59de99211d7f8abc1eba87
babakalikahi72 <babak.alikahi@gmail.com>
2024-08-13 11:57:08 +0430

    add new-record and show in sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -19,17 +19,31 @@ function App() {
     });
   }
 
+  function handelAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject={
+        id: prevState.projects.length + 1,
+        ...projectData,
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+
+  }
+  
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handelAddProject}/>;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handelStartAddProject} />;
   }
 
   return (
     <main className="my-8 flex h-screen gap-8">
-      <ProjectSideBar onStartAddProject={handelStartAddProject} />
+      <ProjectSideBar onStartAddProject={handelStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,25 @@
-import Test from "./Test";
+import { useRef } from "react";
+import Input from "./Input";
 
-const NewProject = () => {
+const NewProject = ({onAdd}) => {
+  const title=useRef();
+  const description=useRef();
+  const dueDate=useRef();
+
+function handelSave() {
+ const enterTitle=title.current.value;
+ const enterDescription=description.current.value;
+ const enterDueDate=dueDate.current.value;
+
+onAdd({
+  title:enterTitle,
+  description:enterDescription,
+  dueDate:enterDueDate,
+ });
+
+}
+
+
   return (
     <div className="mt-16 w-[35rem]">
       <menu className="my-4 flex items-center justify-end gap-4">
@@ -10,15 +29,18 @@ const NewProject = () => {
           </button>
         </li>
         <li>
-          <button className="rounded-md bg-stone-800 px-6 py-2 text-stone-50 hover:bg-stone-950">
+          <button
+           className="rounded-md bg-stone-800 px-6 py-2 text-stone-50 hover:bg-stone-950"
+           onClick={handelSave}
+           >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Test label="Title" />
-        <Test label="Description" textarea />
-        <Test label="Due Date" />
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input ref={dueDate} type="date" label="Due Date" />
       </div>
     </div>
   );
