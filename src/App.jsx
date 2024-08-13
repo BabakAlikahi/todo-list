2d80b62c2d69557dcfa2b3f38348bb391fc300b3
babakalikahi72 <babak.alikahi@gmail.com>
2024-08-13 11:12:18 +0430

    add no-project component and put condition for check have project ot not

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import ProjectSideBar from "./components/ProjectSideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handelStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handelStartAddProject} />;
+  }
+
   return (
-    <main className="my-8 flex h-full gap-8">
-      <ProjectSideBar />
-      <NewProject />
+    <main className="my-8 flex h-screen gap-8">
+      <ProjectSideBar onStartAddProject={handelStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/ProjectSideBar.jsx
+++ src/components/ProjectSideBar.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import Button from "./Button";
 
-const ProjectSideBar = () => {
+const ProjectSideBar = ({ onStartAddProject }) => {
   return (
-    <aside className="w-1/3 rounded-r-xl bg-stone-900 px-8 py-16 text-stone-50 md:w-72">
+    <aside className="w-1/3 rounded-r-xl bg-stone-900 px-8 py-16 text-stone-50 md:w-72 h-full">
       <h2 className="mb-8 font-bold uppercase text-stone-200 md:text-xl">
         your projects
       </h2>
       <div>
-        <button className="rounded-md bg-stone-700 px-4 py-2 text-xs text-stone-400 hover:bg-stone-600 hover:text-stone-100 md:text-base">
-          + Add Project
-        </button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
